257e58ee8e3919833a1ab21a7765799e6a6fa2e9
Ojas Raul <99ojasraul@gmail.com>
2022-05-21 19:21:31 +0530

    Setup for Heroku

--- public/js/app.js
+++ public/js/app.js
@@ -11,7 +11,7 @@ weatherForm.addEventListener('submit', (e) => {
     messageOne.textContent='Loading Forecast...'
     messageTwo.textContent=''
     const location = search.value
-    fetch('http://localhost:3000/weather?address='+location).then((response) => {
+    fetch('/weather?address='+location).then((response) => {
         response.json().then((data) => {
             if (data.error) {
                 return messageOne.textContent=data.error
